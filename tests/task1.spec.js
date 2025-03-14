137de75d08202f9291a713e2d6f5437b02257dfa
anishdaniel0002 <anishdaniel2000@gmail.com>
2025-03-14 11:02:11 +0530

    today

--- tests/task1.spec.js
+++ tests/task1.spec.js
@@ -6,11 +6,26 @@
 
 //multitab
 
-const{test,expect}=require('@playwright/test');
-test ('upload file',async({page})=>{
-    await page.goto("https://davidwalsh.name/demo/multiple-file-upload.php");
+// const{test,expect}=require('@playwright/test');
+// test ('upload file',async({page})=>{
+//     await page.goto("https://davidwalsh.name/demo/multiple-file-upload.php");
 
-    await page.locator('//input[@type="file"]')
+//     await page.locator('//input[@type="file"]')
 
-    await page.waitForTimeout(3000);
+//     await page.waitForTimeout(3000);
+// })
+
+
+//facebook login first page
+
+const{test,expect}=require("@playwright/test");
+test('login fb',async({page})=>{
+    
+   await page.goto("https://m.facebook.com/");
+   let email= await page.locator("//input[@id='email']").fill("anishdaniel");
+   await page.waitForTimeout(3000);
+   let password= await page.locator("(//input[@id='pass'])[1]").fill("daniel");
+   await page.waitForTimeout(3000);
+   
+
 })
