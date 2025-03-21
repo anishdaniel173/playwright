5e79cb5e35199b25c5b5b1fd6b75be275d940c28
anishdaniel0002 <anishdaniel2000@gmail.com>
2025-03-21 10:43:11 +0530

    today

--- tests/task1.spec.js
+++ tests/task1.spec.js
@@ -18,14 +18,28 @@
 
 //facebook login first page
 
-const{test,expect}=require("@playwright/test");
-test('login fb',async({page})=>{
+// const{test,expect}=require("@playwright/test");
+// const { log } = require("console");
+// test('login fb',async({page})=>{
     
-   await page.goto("https://m.facebook.com/");
-   let email= await page.locator("//input[@id='email']").fill("anishdaniel");
-   await page.waitForTimeout(3000);
-   let password= await page.locator("(//input[@id='pass'])[1]").fill("daniel");
-   await page.waitForTimeout(3000);
-   
+//    await page.goto("https://m.facebook.com/");
+//    let email= await page.locator("//input[@id='email']").fill("anishdaniel");
+//    await page.waitForTimeout(3000);
+//    let password= await page.locator("(//input[@id='pass'])[1]").fill("daniel");
+//    await page.waitForTimeout(3000);
 
+//    await page.click("//button[@id='u_0_5_3Q']");
+//    await page.waitForTimeout(3000);
+   
+// })
+//
+
+
+const{test,expect}=require('@playwright/test');
+test ('login myntra',async({page})=>{
+    await page.goto("https://www.myntra.com/");
+    await page.locator("//input[@placeholder='Search for products, brands and more']").fill("men shoe");
+    await page.click ("//span[@class='myntraweb-sprite desktop-iconSearch sprites-search']");
+    
+    await page.waitForTimeout(2000)
 })
